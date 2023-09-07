480d12cf0dd91e6fcc1d99abde010dc7b0de6213
Yeswanth K <88588622+yeswanthkamaraj@users.noreply.github.com>
2023-09-07 21:27:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (48.62%) - LeetSync

---
+++ 2589-maximum-value-of-a-string-in-an-array/maximum-value-of-a-string-in-an-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximumValue(String[] strs) {
+       int ans=0;
+       for(String s:strs){
+           ans=Math.max(ans,x(s));
+       }
+       return ans;
+    }
+    private int x(String s){
+        for(int i=0;i<s.length();++i){
+            if(s.charAt(i)>='a'&& s.charAt(i)<='z'){
+                return s.length();
+            }
+        }
+        return Integer.parseInt(s);
+    }
+}
