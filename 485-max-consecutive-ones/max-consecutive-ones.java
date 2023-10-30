6435bb3ffd73174a273198151d8cddd3415d1bd3
Yeswanth K <88588622+yeswanthkamaraj@users.noreply.github.com>
2023-10-30 22:14:30 +0530

    Time: 3 ms (42.71%) | Memory: 44.1 MB (89.77%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count=0;
+        int result=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                count=count+1;
+                result = Math.max(count, result);
+            }
+            else{
+                count=0;
+            }
+
+        }
+        return result;
+    }
+}
